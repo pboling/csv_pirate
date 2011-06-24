75ac9ff98c00f8454a4e7d73d4ab55d651da6b65
pboling <peter.boling@peterboling.com>
2011-06-23 20:56:07 -0400

    Found an oversight when looking through my code thanks to /r/ruby's constipated comments

--- lib/csv_pirate/the_capn.rb
+++ lib/csv_pirate/the_capn.rb
@@ -421,18 +421,6 @@ module CsvPirate
       plunder
     end
 
-    def data_hash_from_row(row, exclude_id = true, exclude_timestamps = true, example = nil)
-      data_hash = {}
-      my_booty = self.booty.reject {|x| x.is_a?(Hash)}
-      my_booty = exclude_id ? my_booty.reject {|x| a = x.to_sym; [:id, :ID,:dbid, :DBID, :db_id, :DB_ID].include?(a)} : self.booty
-      my_booty = exclude_timestamps ? my_booty.reject {|x| a = x.to_sym; [:created_at, :updated_at, :created_on, :updated_on].include?(a)} : self.booty
-      my_booty = my_booty.reject {|x| !example.respond_to?("#{x}=".to_sym)} unless example.nil?
-      my_booty.each do |method|
-        data_hash = data_hash.merge({method => row[self.pinnacle[self.booty.index(method)]]})
-      end
-      data_hash
-    end
-
     # Grab an old CSV dump (first or last)
     def old_csv_dump(brig)
       file = Dir.entries(self.traverse_board).reject {|x| x.match(/^\./)}.sort.send(brig)
