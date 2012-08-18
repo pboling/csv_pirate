4be405bdff3ee4394b73569156e43d1412212a9f
Peter Boling <peter.boling@gmail.com>
2012-08-18 12:22:49 -0400

    Updated spec_helper to latest rspec defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
-#require 'spec'
-require 'rspec/core'
 require 'csv_pirate'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/spec_helpers/**/*.rb"].each {|f| require f}
 
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  #config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+
 end
 
-# require all files inside spec_helpers
-Dir[File.join(File.dirname(__FILE__), "spec_helpers/*.rb")].each { |file| require file }
